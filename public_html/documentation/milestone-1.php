e204854d5ce62470c77ee3de8d699bd5a264834d
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-22 15:28:31 -0700

    added first persona and his use case

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -14,5 +14,20 @@
 			<p>to document and store all personal work</p>
 			<p>to attract freelance clients</p>
 			<p>to attract those interested in collaboration</p>
+		<h1>Persona Case 1</h1>
+			<p>name: Andy</p>
+			<p>age: 27</p>
+			<p>job: Web-developer/designer at a medium sized company</p>
+			<p>technology: Andy owns an iPhone 7 and the latest model of Macbook Pro.  At work he uses his Macbook in addition to his company provided Mac desktop running OSX.  At home, however, he uses a high powered PC running Windows 8 for personal projects</p>
+			<p>goals: Andy is using my website to explore the design and art I have uploaded.  He is interested in finding collaborators for a online art project and heard about my work from a friend of his</p>
+		<h2>Use Case 1</h2>
+			<ul>
+				<li>Andy logs on to my webpage on his home computer after work and is greeted by a small page with a short biography and relevant information</li>
+				<li>Internal links on the side provide short descriptions/titles of personal work and of paid work</li>
+				<li>Andy clicks on the link named "foo.link" which brings him to a page showing some personal work</li>
+				<li>Intrigued and impressed, Andy moves his mouse to the "contact" link which brings him to a page allowing him to email me</li>
+				<li>Andy writes a short email to me talking about how cool I am and how he'd love to work with me.  He then presses the "send" button</li>
+				<li>Excited about the possibilities, Andy closes my webpage, turns off his computer, and goes to bed for the night</li>
+			</ul>
 	</body>
 </html>
