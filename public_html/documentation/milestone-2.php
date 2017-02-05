d7b46c7fe60d2344809dcce0d49d8b2db5c3f156
Solomon Leyba <solomon.leyba@gmail.com>
2017-02-05 14:18:03 -0700

    update

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,7 +8,7 @@
 		<h1>Content Strategy</h1>
 		<p>I plan to feature images and animations from my own personal <br/> projects as well as links to any professional web development work I've done</p>
 		<h1>Wireframes</h1>
-		<img src="../images/wireframes/desktop_mockup_one.png"/>
+		<img src="../images/wireframes/desktop_mockup_one.png"/><br/>
 		<img src="../images/wireframes/mobile_mockup_one.png"/>
 	</body>
 </html>
