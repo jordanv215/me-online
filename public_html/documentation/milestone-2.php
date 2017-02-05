3135fc5b984d30f7f34c97e63702e897fd8dce62
Solomon Leyba <solomon.leyba@gmail.com>
2017-02-05 14:17:13 -0700

    added wireframes and content strategy descriptor

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset="UTF-8">
+		<title>MyOnlineContentStrategy.info</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<p>I plan to feature images and animations from my own personal <br/> projects as well as links to any professional web development work I've done</p>
+		<h1>Wireframes</h1>
+		<img src="../images/wireframes/desktop_mockup_one.png"/>
+		<img src="../images/wireframes/mobile_mockup_one.png"/>
+	</body>
+</html>
