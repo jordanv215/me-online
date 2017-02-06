eaaea39df2c3dae7f3e377189c2a98444d65e4e8
Solomon Leyba <solomon.leyba@gmail.com>
2017-02-06 08:49:12 -0700

    updated again oops forgot to push

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,6 +7,10 @@
 	<body>
 		<h1>Content Strategy</h1>
 		<p>I plan to feature images and animations from my own personal <br/> projects as well as links to any professional web development work I've done</p>
+		<p>About me: short bio or statement</p>
+		<p>Professional: to include any paid work</p>
+		<p>Personal - real life: for images of any physical work of mine</p>
+		<p>Personal - online: for image/ animations that only exists digitally</p>
 		<h1>Wireframes</h1>
 		<img src="../images/wireframes/desktop_mockup_one.png"/><br/>
 		<img src="../images/wireframes/mobile_mockup_one.png"/>
