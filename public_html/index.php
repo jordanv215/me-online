f706070dce5a06662ffdc7d5d02ad82579bef8fc
Solomon Leyba <solomon.leyba@gmail.com>
2017-02-09 16:24:35 -0700

    updating css rules

--- public_html/index.php
+++ public_html/index.php
@@ -2,24 +2,31 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Failed To Load Page</title>
+		<title>Failed To Load</title>
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:100i,400" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="styles/style.css" />
 	</head>
 	<body>
-		<div class="header">
+		<div id="header">
+			<h1>Me-Online.info</h1>
+		</div>
+		<div id="menu">
+			<ul>
+				<li>Root</li>
+				<li>Content Group 1</li>
+				<li>Content Group 2</li>
+				<li>Content Group 3</li>
+				<li>Contact</li>
+			</ul>
+		</div>
+		<!--<div id="body">
 
 		</div>
-		<div class="menu">
+		<div id="statement">
 
 		</div>
-		<div class="body">
+		<div id="evidence">
 
-		</div>
-		<div class="statement">
-
-		</div>
-		<div class="evidence">
-
-		</div>
+		</div>-->
 	</body>
 </html>
