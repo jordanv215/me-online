77fb03bcb264a0eee2bc6d06a9e1f40bf6512453
Solomon Leyba <solomon.leyba@gmail.com>
2017-02-09 13:52:37 -0700

    added a few preliminary divs to index.php, added stylesheet

--- public_html/index.php
+++ public_html/index.php
@@ -3,8 +3,23 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Failed To Load Page</title>
+		<link rel="stylesheet" type="text/css" href="styles/style.css" />
 	</head>
 	<body>
+		<div class="header">
 
+		</div>
+		<div class="menu">
+
+		</div>
+		<div class="body">
+
+		</div>
+		<div class="statement">
+
+		</div>
+		<div class="evidence">
+
+		</div>
 	</body>
 </html>
