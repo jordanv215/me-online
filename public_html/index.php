2cf2fc3a13b76787603d2ea89a8571cd8d331ee1
Solomon Leyba <solomon.leyba@gmail.com>
2017-02-13 15:51:28 -0700

    added links for jQuery and Bootstrap

--- public_html/index.php
+++ public_html/index.php
@@ -2,31 +2,37 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Failed To Load</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- Google fonts import -->
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:100i,400" rel="stylesheet">
+
+		<!-- my custom CSS -->
 		<link rel="stylesheet" type="text/css" href="styles/style.css" />
+
+		<title>Failed To Load</title>
 	</head>
 	<body>
-		<div id="header">
-			<h1>Me-Online.info</h1>
-		</div>
-		<div id="menu">
-			<ul>
-				<li>Root</li>
-				<li>Content Group 1</li>
-				<li>Content Group 2</li>
-				<li>Content Group 3</li>
-				<li>Contact</li>
-			</ul>
-		</div>
-		<!--<div id="body">
+		<header>
+			<div class="col-xs-12">
 
-		</div>
-		<div id="statement">
+			</div>
+		</header>
+		<main>
 
-		</div>
-		<div id="evidence">
+		</main>
+		<footer>
 
-		</div>-->
+		</footer>
 	</body>
 </html>
